44c1a0437a14fb346a1ffcd6324f094ac1f15ab0
Ajinkya Chatur <ajinkya.chatur20596@gmail.com>
2021-12-23 14:39:27 -0800

    Footer Added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import Cards from '../js/Cards';
+import Footer from '../js/Footer';
 import HeroSection from '../js/HeroSection';
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 }
