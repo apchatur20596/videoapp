b2d4e7d745827fd25a85445b646d3bfd21f444e0
Ajinkya Chatur <ajinkya.chatur20596@gmail.com>
2021-12-23 14:36:34 -0800

    cards added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../js/Cards';
 import HeroSection from '../js/HeroSection';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
