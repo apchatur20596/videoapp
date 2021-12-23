b2d42404d08294b3edd2e1db26ee97525e31f83a
Ajinkya Chatur <ajinkya.chatur20596@gmail.com>
2021-12-23 14:14:28 -0800

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/js/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </Router>
     </div>
   );
 }
